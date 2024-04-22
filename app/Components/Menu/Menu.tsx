1204fb7782f74a6579c5c9f88a9d1f3ea9c72406
Bonkesam <bonkesam9@gmail.com>
2024-04-22 23:04:51 +0300

    Wraps up the menu

---
+++ app/Components/Menu/Menu.tsx
@@ -0,0 +1,45 @@
+'use client';
+import { AnimatePresence, motion } from 'framer-motion';
+import React, { useState } from 'react'
+import Button from './Button/Button';
+import Nav from './Nav/Nav';
+import styles from "./style.module.scss"
+
+const menu = {
+    open: {
+        width: "480px",
+        height: "650px",
+        top: "-25px",
+        right: "-25px",
+        transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1]}
+    },
+    closed: {
+        width: "100px",
+        height: "40px",
+        top: "0px",
+        right: "0px",
+        transition: { duration: 0.75, delay: 0.35, type: "tween", ease: [0.76, 0, 0.24, 1]}
+    }
+}
+
+const Menu = () => {
+    const [isActive, setIsActive] = useState(false);
+
+    return (
+        <div className={styles.header}>
+            <motion.div 
+                className={styles.menu}
+                variants={menu}
+                animate={isActive ? "open" : "closed"}
+                initial="closed"
+            >
+                <AnimatePresence>
+                    {isActive && <Nav />}
+                </AnimatePresence>
+            </motion.div>
+            <Button isActive={isActive} toggleMenu={() => {setIsActive(!isActive)}}/>
+        </div>
+    )
+}
+
+export default Menu;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Menu from "./Components/Menu/Menu";
 
 export default function Home() {
   return (
   <main>
+    <Menu/>
     Hello
   </main>
   );
